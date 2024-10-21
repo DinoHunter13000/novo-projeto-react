fef91b8b08f77859a963b96be46c60d9ab123507
Isaac <zacob08@gmail.com>
2024-10-21 19:42:57 -0400

    home alterada

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom";
 import Button from '@mui/material/Button';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 const Home = () => {
   let { contatoId } = useParams();
@@ -7,10 +10,10 @@ const Home = () => {
     <>
     <main>
       <div className="caixa_de_login">
-          <label htmlFor="Email">Email</label>
-          <input className="input_email" placeholder="Digite aqui o seu email" type="email" name="email" id="email" required />
-          <label htmlFor="Senha">Senha</label>
-          <input className="input_senha" placeholder="Digite aqui a sua senha" type="password" name="password" id="password" required /> 
+          <label htmlFor="Email">Email: </label>
+          <TextField id="outlined-basic" label="Email" variant="outlined" required type="email" name="email" itemID="email" size="small"/>
+          <label htmlFor="Senha">Senha: </label>
+          <TextField id="outlined-basic" label="Senha" variant="outlined" required type="email" name="email" itemID="email" size="small"/>
           <Button variant="contained" color="success">Logar</Button>
       </div>
     </main>
