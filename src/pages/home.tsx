7cffe4e5dbb93888a2f08ca30f197e7697f0f4cc
Isaac <zacob08@gmail.com>
2024-10-25 21:42:23 -0400

    continua em casa por favor

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import '../style/estilos.css'
+import { Margin } from "@mui/icons-material";
 
 const Home = () => {
   let { contatoId } = useParams();
@@ -10,11 +11,13 @@ const Home = () => {
     <main>
       <div className="login">
           <label htmlFor="Email">Email: </label>
-          <TextField id="outlined-basic" label="Email" variant="outlined" required type="email" name="email" itemID="email" size="small" className="input_email"/>
+          <TextField id="outlined-basic" label="Email" variant="outlined" required type="email" 
+          name="email" itemID="email" size="small"sx={{margin: "0.5em 0 1em 0"}}/>
           <label htmlFor="Senha">Senha: </label>
-          <TextField id="outlined-basic" label="Senha" variant="outlined" required type="password" name="password" itemID="password" size="small" className="input_senha"/>
-          <label>Esqueceu a senha? </label>
-          <div className="botao"><Button variant="contained" color="success">Logar</Button></div>
+          <TextField id="outlined-basic" label="Senha" variant="outlined" required type="password" 
+          name="password" itemID="password" size="small" sx={{margin: "0.5em 0"}}/>
+          <label className="label">Esqueceu a senha? </label>
+          <Button variant="contained" color="success" sx={{margin: "0.5em 0", display: "flex"}}>Logar</Button>
       </div>
     </main>
     </>
